bc9fd230401f14de0a3b4685bb6563e085d5c6c0
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-09-30 19:02:05 -0700

    Adding in initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hunters::Application.config.secret_token = 'd8ec16095c9dd086e24d0c77e8650392198106efc510b574181ee71f4c9ce88ca8325d00fc22079c1e91ad9277109cb2b4a55f8896643ead9f594219701f1ce0'
