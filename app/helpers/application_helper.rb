0e744a243a28e39689bd51f8ccdab6f57e6830fe
Mustafa Tasdemir <mustafa.tasdemir@hotmail.com.tr>
2013-10-02 12:55:24 -0400

    dynamic title helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	# Returns the page title dynamically
+	def full_title(page_title)
+
+		#Default Title Prefix
+		base_title = "BestBay"
+		
+		if(page_title.empty?)
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
