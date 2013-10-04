49e2fec7e721dac0b9515ba5729ce190f2f4b863
Mustafa Tasdemir <mustafa.tasdemir@hotmail.com.tr>
2013-10-03 21:14:36 -0400

    User signin test cases are added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @user = User.new(email: "user@example.com", password: "password") }
+	
+  subject { @user }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+
+  describe "when email is not available" do
+    before { @user.email = "" }
+    it { should_not be_valid }
+  end
+
+  describe "when password does not match with the valid email" do
+    before { @user.password = "example@example.com", @user.password = "new_password " }
+    it { should_not be_valid }
+  end
+
+
+  describe "when the email and password combination is valid" do
+      before do 
+      	@user.email = "example@example.com"
+      	@user.password = "password"
+      end
+      it { should be_valid }
+  end
+
+
+
 end
