0bcc976e44d74c482d0270c67d56f201ed235620
Jigar Patel <jigarbjpatel@gmail.com>
2013-10-10 16:21:41 -0400

    Changed Index.html.erb_spec.rb

--- spec/views/static_pages/index.html.erb_spec.rb
+++ spec/views/static_pages/index.html.erb_spec.rb
@@ -7,14 +7,4 @@ describe "static_pages/index" do
       stub_model(StaticPage)
     ])
   end
-
-<<<<<<< HEAD
-
-=======
-  it "renders a list of static_pages" do
-    #FIXME Mustafa render static pages  gives authentication error
-    #render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-  end
->>>>>>> e070de61dad59de42f6243b8070a78751b39e2d9
 end
