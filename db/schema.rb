13c35656f8501a95358d650e5b28779f04866d5d
Jigar Patel <jigarbjpatel@gmail.com>
2013-10-11 04:40:22 -0400

    Merge Conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,6 @@ ActiveRecord::Schema.define(:version => 20131011111535) do
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "data_files", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "products", :force => true do |t|
     t.string   "title"
     t.integer  "category_id"
