78a41e82fa3dc4020475b80901729a3c6a71a242
Mustafa Tasdemir <mustafa.tasdemir@hotmail.com.tr>
2013-10-13 17:32:07 -0400

    Category test cases updated

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe Category do
+
+	before { @category = Category.new(name: "Sample Category", description: "Sample Category Explanation") }
+	
+    subject { @category }
+    it { should respond_to(:name) }
+    it { should respond_to(:description) }
+
+
+    describe "when name is not available" do
+      before { @category.name = "" }
+      it { should_not be_valid }
+    end
+
+    describe "when description is not available" do
+      before { @category.description = "" }
+      it { should be_valid }
+    end
+
+
 end
