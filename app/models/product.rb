d8cac5709002575f94a1147391ed45dc0236046b
SusanaLauH <susanalauhou@hotmail.com>
2013-10-13 11:30:23 -0700

    Update validations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ActiveRecord::Base
-
   attr_accessible :category_id, :condition, :fixed_price, :id, :is_bid, :starting_price, :title, :brand, :ptype, :listing_days, :start_date, :shipping_cost, :image, :remote_image_url, :days_used
 
   belongs_to :category
@@ -7,16 +6,32 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   #delegate :name, :to => :category, :prefix => true
-  #validates :fixed_price,
-   #         numericality: { greater_than_or_equal_to: 0,
-    #                        less_than_or_equal_to: 1000000 }
-  #validates :starting_price, presence: true,
-   #         numericality: { greater_than_or_equal_to: 0,
-    #                        less_than_or_equal_to: 1000000 }
-  #validates :shipping_cost, presence: true,
-   #         numericality: { greater_than_or_equal_to: 0,
-    #                        less_than_or_equal_to: 1000000 }
 
+
+  #To validate the fixed price only when bidding is not selected
+  validates :fixed_price,
+           numericality: { greater_than_or_equal_to: 0,
+                          less_than_or_equal_to: 1000000 },
+           :unless => :is_bid?
+
+  #To validate the starting price only when bidding is selected
+  validates :starting_price, presence: true,
+           numericality: { greater_than_or_equal_to: 0,
+                          less_than_or_equal_to: 1000000 },
+           :if => :is_bid?
+
+  #To validate that shipping cost is not left blank and that it is between $0 and $1,000,000.00
+  validates :shipping_cost, presence: true,
+           numericality: { greater_than_or_equal_to: 0,
+                          less_than_or_equal_to: 1000000 }
+
+  #To validate the format of the uploaded image
+  validates :image, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|png)\Z}i,
+      message: 'Image must be a .gif, ,jpg or .png file'
+  }
+
+  #  Comment
   def resetDaysUsedIfNewProduct
     if self.condition == true
       self.days_used = ""
