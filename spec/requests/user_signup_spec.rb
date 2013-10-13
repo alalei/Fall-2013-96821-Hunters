aa9bc629b3ee2e7e075ed313be42d85d97e8d1c6
alalei <leixuping@gmail.com>
2013-10-13 18:40:10 -0400

    add user_signup_spec.rb

---
+++ spec/requests/user_signup_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe 'UserPages' do
+  subject { page }
+
+  describe 'Sign up' do
+    before do
+      visit new_user_registration_path
+    end
+
+    let(:submit) { 'Sign up' }
+
+    describe 'without information' do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe 'without password' do
+      before {fill_in 'user_email', with: 'user@example.com'}
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe 'without user_email' do
+      before {fill_in 'user_password',with: 'password2'}
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe 'with unmatched password and confirmation password' do
+      before {fill_in 'user_password',with: 'password2'}
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe 'with valid information' do
+      before do
+        fill_in 'user_email',     with: 'user@example.com'
+        fill_in 'user_password',  with: 'password2'
+        fill_in 'user_password_confirmation', with: 'password2'
+      end
+
+      it 'should create a user' do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe 'after saving the user' do
+        before { click_button submit}
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_content(user.email) }
+      end
+
+    end
+
+  end
+end
